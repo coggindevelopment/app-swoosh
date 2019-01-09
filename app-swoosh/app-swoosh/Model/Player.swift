556672a6828fc1cda6bc0b5d7620b585dbb5d03f
Nicholas Coggin <nicholascoggin@Nicholass-MacBook-Pro.local>
2019-01-08 18:26:52 -0600

    lesson-09 commit - IBActions & Data Models

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Nicholas Coggin on 1/8/19.
+//  Copyright © 2019 Nicholas Coggin. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
